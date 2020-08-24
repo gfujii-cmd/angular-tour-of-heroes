7427200c05750b46d935ad761db08555c66591e4
Guilherme Fujii <gui.fujii2000@gmail.com>
2020-08-24 16:32:33 -0300

    CreateHeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; //Importing the hero interface
 import { HEROES } from '../mock-heroes'; // Importing the mock list of heroes to treat
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
